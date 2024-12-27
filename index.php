cc517582b6ce431f8e50fb436b0d8cf4185c3cb1
AdiRianto03 <Adotcasfer24@gmail.com>
2024-12-27 17:56:02 +0700

    fiks project

--- index.php
+++ index.php
@@ -155,7 +155,3 @@ $result_project = $conn->query($sql_project);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
-
-
-<!-- cek cek -->
